41fe23b57d6ad8c7191a37f53d232abeae30775a
DoggoX <87870418+DoggoX@users.noreply.github.com>
2025-03-27 02:41:05 +0100

    up!

---
+++ Csgo-version-net/Program.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Csgo_version_net
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Title = "CS:GO";
+
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Console.WriteLine("Ez a rendszer nem támogatott.");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Verzió átírása...");
+            RegistryKey steamrk = Registry.CurrentUser.OpenSubKey("Software\\Valve\\Steam", false);
+            if (steamrk == null)
+            {
+                Console.WriteLine("Valami hiba lépett fel.");
+                Console.ReadKey();
+                return;
+            }
+            string steamPath = (string)steamrk.GetValue("SteamPath");
+            if (steamPath == null)
+            {
+                Console.WriteLine("Valami hiba lépett fel.");
+                Console.ReadKey();
+                return;
+            }
+            string csFolder = steamPath + "/steamapps/common/Counter-Strike Global Offensive/csgo";
+            string steamInf = csFolder + "/steam.inf";
+            Console.WriteLine("# " + steamInf);
+            Console.WriteLine("");
+            try
+            {
+                var allLines = File.ReadAllLines(steamInf);
+                Console.WriteLine("(1) " + allLines[0] + " --> " + "ClientVersion=2000258");
+                Console.WriteLine("(2) " + allLines[1] + " --> " + "ServerVersion=2000258");
+                allLines[0] = "ClientVersion=2000258";
+                allLines[1] = "ServerVersion=2000258";
+                File.WriteAllLines(steamInf, allLines);
+                Console.WriteLine("\nVerzió átírva.\n\n -> Kérlek válaszd ki a \"Play Legacy Version of CS:GO\"-t!");
+                Process.Start("steam://launch/730/dialog");
+                Thread.Sleep(850);
+                MessageBox.Show("Kérlek válaszd ki a CS:GO-t és inditsd el!", "cshungary.fun");
+                Thread.Sleep(200);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Valami hiba lépett fel. ");
+                Console.Write(" -> " + ex.Message);
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
---
+++ Csgo-version-net/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Csgo Version")]
+[assembly: AssemblyDescription("Csgo-hoz átírja a fájlok között a verziót, hogy játszhass, raktáradat betöltse.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DoggoStudio")]
+[assembly: AssemblyProduct("Csgo Version Setup")]
+[assembly: AssemblyCopyright("Copyright © DoggoStudio 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dc03ef79-afc2-49b0-ba89-de7b3d3f78da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.7")]
+[assembly: NeutralResourcesLanguage("hu-HU")]
